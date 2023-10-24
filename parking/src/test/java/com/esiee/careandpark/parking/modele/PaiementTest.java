5b988d1075a201c187b75de2740193d27929db39
am.aimeur@gmail.com <am.aimeur@gmail.com>
2023-10-24 18:41:57 +0200

    Modification pour les tests de la classe paiement

--- parking/src/test/java/com/esiee/careandpark/parking/modele/PaiementTest.java
+++ parking/src/test/java/com/esiee/careandpark/parking/modele/PaiementTest.java
@@ -5,11 +5,8 @@ import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
-
 import com.esiee.careandpark.parking.modele.reference.TypePlace;
-
 import java.util.Map;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PaiementTest {
@@ -41,7 +38,6 @@ public class PaiementTest {
             // Affichele texte extrait et la date et l'heure d'entrée 
             System.out.println("Heure d'entree dans le parking : " + premiereHeure);
             
-            
             // Heure de sortie (2 élements dans la liste)________________________________________________________________________________
 
             // Pause de 10 minutes avant le deuxième appel 
@@ -63,19 +59,84 @@ public class PaiementTest {
             // Affichez la deuxième heure dans la console
             System.out.println("Date/Heure d'entree et de sortie du parking : " + heureList );
 
-
             // Calcul du prix______________________________________________________________________________________
+
+            // CALCUL DU PRIX POUR UN BUS
             // Appelez la nouvelle fonction calculerPrix pour obtenir le prix
-            double prix = paiement.calculerPrix(extractedText,TypePlace.BUS);
+            double prixBus = paiement.calculerPrix(extractedText,TypePlace.BUS);
             // Affichez le prix dans la console
-            System.out.println("Prix calcule : " + prix+" euros");
-            // Vérifiez que le prix est correct (50 euros pour 10 heures)
-            assertEquals(8.91, prix);
+            System.out.println("Prix calcule : " + prixBus+" euros");
+            assertEquals(8.91, prixBus);
+
+            // CALCUL DU PRIX POUR UNE PLACE HANDICAPE
+            double prixHandicape = paiement.calculerPrix(extractedText,TypePlace.HANDICAPE);
+            // Affichez le prix dans la console
+            System.out.println("Prix calcule : " + prixHandicape+" euros");
+            assertEquals(2.97, prixHandicape);
 
             // Test dans le cas où une fois sortie la voiture re-rentre________________________________________________________________
             extractedText = paiement.ia(imageName);
             heureList = plaqueTexteHeureMap.get(extractedText);
             System.out.println("Test voiture rerentre - contenu de la liste : " + heureList );
+
+        } catch (Exception e) {
+            // Gérez les exceptions ici
+            e.printStackTrace();
+            fail("Une exception s'est produite");
+        }
+    }
+
+    @Test
+    public void calculerPrixTest() {
+        // Plaque d'immatriculation
+        String imageName = "C:\\Users\\amaim\\OneDrive\\Documents\\ESIEE\\E5\\Capgemini\\projet_parkstronauts\\parking\\src\\test\\java\\com\\esiee\\careandpark\\parking\\modele\\plaque.png";
+
+        // Crée une instance de la classe Payement
+        Paiement paiement = new Paiement();
+
+        try {
+            // extraire le texte de l'image
+            String extractedText = paiement.ia(imageName);
+            // Vérifie si la clé existe dans la map
+            Map<String, List<LocalDateTime>> plaqueTexteHeureMap = paiement.getPlaqueTexteHeureMap();
+            // Récupére la liste contenant les dates/heures correspondante à la clé
+            List<LocalDateTime> heureList = plaqueTexteHeureMap.get(extractedText);
+            LocalDateTime premiereHeure = heureList.get(0);
+            // Affichele texte extrait et la date et l'heure d'entrée 
+            System.out.println("Heure d'entree dans le parking : " + premiereHeure);
+            
+            // Heure de sortie (2 élements dans la liste)________________________________________________________________________________
+            // Créez une deuxième heure simulée, 10 heures après la première 
+            LocalDateTime deuxiemeHeureSimulee = premiereHeure.plus(5, ChronoUnit.HOURS);
+            // Ajoutez la date et l'heure correspondant à la sortie du parking à la liste
+            heureList.add(deuxiemeHeureSimulee);
+
+            // Calcul du prix______________________________________________________________________________________
+
+            // CALCUL DU PRIX POUR UN BUS
+            // Appelez la nouvelle fonction calculerPrix pour obtenir le prix
+            double prixBus = paiement.calculerPrix(extractedText,TypePlace.BUS);
+            // Affichez le prix dans la console
+            System.out.println("Prix calcule : " + prixBus+" euros");
+            assertEquals(8.91*6, prixBus);
+
+            // CALCUL DU PRIX POUR UNE PLACE HANDICAPE
+            double prixHandicape = paiement.calculerPrix(extractedText,TypePlace.HANDICAPE);
+            // Affichez le prix dans la console
+            System.out.println("Prix calcule : " + prixHandicape+" euros");
+            assertEquals(2.97*6, prixHandicape);
+
+            // CALCUL DU PRIX POUR UNE PLACE Deux roues
+            double prixDeuxRoues = paiement.calculerPrix(extractedText,TypePlace.DEUX_ROUES);
+            // Affichez le prix dans la console
+            System.out.println("Prix calcule : " + prixDeuxRoues+" euros");
+            assertEquals(1.485*6, prixDeuxRoues);
+
+            // CALCUL DU PRIX POUR UNE PLACE NORMALE
+            double prixNominale = paiement.calculerPrix(extractedText,TypePlace.NOMINALE);
+            // Affichez le prix dans la console
+            System.out.println("Prix calcule : " + prixNominale+" euros");
+            assertEquals(4.455*6, prixNominale);
 
         } catch (Exception e) {
             // Gérez les exceptions ici
